b94f50c105f398d3af917dd5c5ff0760198fcf22
Diogo Oliveira <dihhg35@gmail.com>
2020-05-04 18:02:03 -0300

    adicionando aula 3 completa

--- aula03/metodos.php
+++ aula03/metodos.php
@@ -16,6 +16,7 @@
                 $cidade = $_POST["inputCidade"];
                 $estado = $_POST["inputEstado"];
                 $cep = $_POST["inputCep"];
+                $aceitou = $_POST["inputAceite"];
             ?>
 
             <article>
@@ -48,29 +49,50 @@
             </article>
 
         <?php elseif ($_GET && isset($_GET)): ?>
-        
+
+                
+            <?php 
+                $nome = &$_GET["inputNome"];
+                $email = &$_GET["inputEmail"];
+                $endereco = &$_GET["inputSenha"];
+                $senha = &$_GET["inputEndereco1"];
+                $cidade = &$_GET["inputCidade"];
+                $estado = &$_GET["inputEstado"];
+                $cep = &$_GET["inputCep"];
+                $aceitou = &$_GET["inputAceite"];
+
+                
+            ?>
+            
             <article>
                 <h2>Respostas Recebidas por GET:</h2>
 
+                <table class="table">
+
                 <thead class="thead-dark">
                     <tr>
-                        <th scope="col">nome</th>
-                        <th scope="col">email</th>
-                        <th scope="col">senha</th>
-                        <th scope="col" colspan="2">endereço</th>
+                        <th scope="col">Nome</th>
+                        <th scope="col">Email</th>
+                        <th scope="col">Endereço</th>
+                        <th scope="col" colspan="2">Senha</th>
                         <th scope="col">Cidade - UF</th>
+                        <th scope="col">Estado</th>
                         <th scope="col">CEP</th>
+                        <th scope="col">Política de Privacidade</th>
                     </tr>
                 </thead>
                 <tbody>
                     <!-- DESAFIO: INCLUA CADA UM DOS CAMPOS UTILIZANDO O QUE JÁ APRENDEMOS EM PHP -->
+                    
                     <tr>
-                    <th scope="row">Fulano</th>
-                    <td>fulano@dasilva.com</td>
-                    <td>12345678</td>
-                    <td colspan="2">Rua Cardoso de Melo, 123 - Vila Olímpia - Prédio Digital House - Sala 12</td>
-                    <td>São Paulo - SP</td>
-                    <td>12345-678</td>
+                    <th scope="row"><?= $nome; ?></th>
+                    <td><?= $email; ?></td>
+                    <td><?= $senha; ?></td>
+                    <td colspan="2"><?= $endereco; ?></td>
+                    <td><?= $cidade; ?></td>
+                    <td><?= $estado; ?></td>
+                    <td><?= $cep; ?></td>
+                    <td><?= $aceitou == "on" ? "aceitou" : "não aceitou" ; ?></td>
                     </tr>
                 </tbody>
                 </table>
@@ -80,7 +102,7 @@
         
             <article class="alert alert-warning col-6 mx-auto" role="alert">
                 <p><b>Ops... parece que nenhum dado foi recebido!</b></p>
-                <p>Por favor, preencha o formulário <a href="11-metodo-GET.php" title="Acessar o Formulário GET" rel="next">GET</a> ou o formulário <a href="11-metodo-POST.php" title="Acessar o Formulário POST" rel="next">POST</a>.</p>
+                <p>Por favor, preencha o formulário <a href="11-metodo-get.php" title="Acessar o Formulário GET" rel="next">GET</a> ou o formulário <a href="11-metodo-post.php" title="Acessar o Formulário POST" rel="next">POST</a>.</p>
                 <p>Se você acabou de preencher o formulário, foi direcionado para essa página e está vendo essa mensagem...<br/>deve ter algum erro no seu código! Nesse caso... bora debugar! hehehe</p>
             </article>
 
